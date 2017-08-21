0f8d064c07516f9440c205a2d5fe6801abd05d97
arturorenteriia <arturorb91@gmail.com>
2017-08-20 19:03:32 -0500

    update

---
+++ public/images/routes/web.php
@@ -0,0 +1,222 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('tours', function () {
+    return view('tours');
+});
+Route::get('welcome', function () {
+    return view('welcome');
+});
+Route::get('restaurants', function () {
+    return view('restaurants');
+});
+Route::get('advertise', function () {
+    return view('advertise');
+});
+Route::get('about', function () {
+    return view('about');
+});
+Route::get('recommended', function () {
+    return view('recommended');
+});
+Route::get('history', function () {
+    return view('history');
+});
+Route::get('safety', function () {
+    return view('safety');
+});
+Route::get('beaches', function () {
+    return view('beaches');
+});
+Route::get('retire', function () {
+    return view('retire');
+});
+Route::get('tipping', function () {
+    return view('tipping');
+});
+Route::get('taxis', function () {
+    return view('taxis');
+});
+Route::get('drinking', function () {
+    return view('drinking');
+});
+Route::get('speak', function () {
+    return view('speak');
+});
+Route::get('temperature', function () {
+    return view('temperature');
+});
+Route::get('weddings', function () {
+    return view('weddings');
+});
+Route::get('driving', function () {
+    return view('driving');
+});
+Route::get('rentjeep', function () {
+    return view('rentjeep');
+});
+Route::get('surfing', function () {
+    return view('surfing');
+});
+Route::get('tequilatours', function () {
+    return view('tequilatours');
+});
+Route::get('cookingclass', function () {
+    return view('cookingclass');
+});
+Route::get('boattours', function () {
+    return view('boattours');
+});
+Route::get('ziplining', function () {
+    return view('ziplining');
+});
+Route::get('pirateship', function () {
+    return view('pirateship');
+});
+Route::get('fishing', function () {
+    return view('fishing');
+});
+Route::get('golf', function () {
+    return view('golf');
+});
+Route::get('winatrip', function () {
+    return view('winatrip');
+});
+Route::get('rythms', function () {
+    return view('rythms');
+});
+Route::get('caletas', function () {
+    return view('caletas');
+});
+Route::get('sansebastian', function () {
+    return view('sansebastian');
+});
+Route::get('ziplinetour', function () {
+    return view('ziplinetour');
+});
+Route::get('nightlife', function () {
+    return view('nightlife');
+});
+Route::get('colomitos', function () {
+    return view('colomitos');
+});
+Route::get('cafedesartistes', function () {
+    return view('cafedesartistes');
+});
+Route::get('lasanta', function () {
+    return view('lasanta');
+});
+
+Route::get('mexicanfood', function () {
+    return view('mexicanfood');
+});
+
+Route::get('italianfood', function () {
+    return view('italianfood');
+});
+
+Route::get('flights', function () {
+    return view('flights');
+});
+
+Route::get('hotels', function () {
+    return view('hotels');
+});
+
+Route::get('laleche', function () {
+    return view('laleche');
+});
+Route::get('marietas', function () {
+    return view('marietas');
+});
+Route::get('medicaltourism', function () {
+    return view('medicaltourism');
+});
+Route::get('realestate', function () {
+    return view('realestate');
+});
+Route::get('alltours', function () {
+    return view('alltours');
+});
+Route::get('gaypv', function () {
+    return view('gaypv');
+});
+Route::get('terms', function () {
+    return view('terms');
+});
+Route::get('clubs', function () {
+    return view('clubs');
+});
+Route::get('privacy', function () {
+    return view('privacy');
+});
+Route::get('map', function () {
+    return view('map');
+});
+Route::get('sportsbars', function () {
+    return view('sportsbars');
+});
+Route::get('stripclubs', function () {
+    return view('stripclubs');
+});
+Route::get('discount', function () {
+    return view('discount');
+});
+Route::get('personaldriver', function () {
+    return view('personaldriver');
+});
+Route::get('jetboat', function () {
+    return view('jetboat');
+});
+Route::get('yachtcharter', function () {
+    return view('yachtcharter');
+});
+Route::get('sunsetcruises', function () {
+    return view('sunsetcruises');
+});
+Route::get('cosmeticsurgeon', function () {
+    return view('cosmeticsurgeon');
+});
+Route::get('facelift', function () {
+    return view('facelift');
+});
+Route::get('breastimplants', function () {
+    return view('breastimplants');
+});
+Route::get('liposuction', function () {
+    return view('liposuction');
+});
+Route::get('facialfillers', function () {
+    return view('facialfillers');
+});
+Route::get('nosejob', function () {
+    return view('nosejob');
+});
+Route::get('jointreplacement', function () {
+    return view('jointreplacement');
+});
+Route::get('fullmedical', function () {
+    return view('fullmedical');
+});
+Route::get('obgyn', function () {
+    return view('obgyn');
+});
+Route::get('contact', function () {
+    return view('contact');
+});
+Route::get('uber', function () {
+    return view('uber');
+});
