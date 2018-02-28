d09118f4bc83e8fe004c4e3ad4d309a8e3653cbf
arturorenteriia <arturorb91@gmail.com>
2018-02-27 20:04:20 -0600

    s
    update

---
+++ resources/views/fiestaamericana.blade.php
@@ -0,0 +1,36 @@
+@extends('master')
+@section('body')
+
+
+                  <h3><center>Fiesta Americana Puerto Vallarta All Inclusive & Spa</center>
+               </h3>
+               The charm and the warmth of this destination make Fiesta Americana, Puerto Vallarta All Inclusive, one of the most attractive 5 star  hotels in Bahía de Banderas.
+
+The Mexican architecture blends traditional with contemporarary styles. Based in the Mexican state of Jalisco with it’s vibrant culture, beautiful beaches and nature, you will feel worlds away from the stresses of everyday life. The buildings harmonize perfectly with the landscape surrounding the Western Sierra Madre Mountains, and the most beautiful beach in Puerto Vallarta is on your doorstep. Relax in the shade of the largest thatched-palm ''palapa'' roof in the region, admire the beautiful waterfall, and rest in comfortable rooms with functional suites. An environment that favors total relaxation.
+
+To ensure that your stay is complete,food experiences and family fun are included.  
+Our restaurants  will showcase their culinary delights, both regional and international.
+
+Parking is exclusively for cars. Other vehicles such as buses, trailers, buggies, motorcycles, quad-bikes, are not permitted to park inside the hotel car park.
+
+Fiesta Americana Puerto Vallarta All Inclusive & Spa is an Eco-friendly hotel, so it is a 100% smoke free hotel.
+
+Please remember that check-in time is 3pm, subject to availability. Check-out time is 12pm; we suggest you use our express check-out service.
+                <div class="col-sm-6 col-lg-4 col-md-4 photography graphicdesign "style="float: left;">
+                     <div class="gal-detail thumb box-shadow">
+                        <img src="images/tipping1.jpeg" class="thumb-img" alt="work-thumbnail">
+                        </a></div>
+                        <br>
+                     <br>
+                  </div>
+
+
+@stop
+
+@section('sidebar')
+Aqui va el texto o codigo que quieras debajo esta imagen
+@stop
+@section('sidebar2')
+Aqui va el texto o codigo que quieras debajo esta segunda imagen
+@stop
+
